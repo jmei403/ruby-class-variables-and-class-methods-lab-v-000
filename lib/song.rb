640b33873a9d724323d0bfd1d5330dfab84a33d1
Jason Mei <jason.mei403@gmail.com>
2018-06-14 13:12:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,15 +2,22 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
+  @@artist = []
+  @@genres = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@genres << genre unless @genres.include?(genre)
   end
 
   def count
     @@count
+  end
+
+  def genres
+    @@genres
   end
 end
