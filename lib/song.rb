1658c006f6fb4a25d51b8b566a3212ef8aa06a4d
Jason Mei <jason.mei403@gmail.com>
2018-06-14 13:14:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,6 +10,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
+    @@artists << artist unless @@artists.include?(artist)
     @@genres << genre unless @@genres.include?(genre)
   end
 
